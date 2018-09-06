1e83fc5c9995e8fd8a7c399a43b7872a325444e6
Joseph ONeill <oneilljo@appstate.edu>
2018-09-06 14:34:41 -0600

    Removing all of the Reports that I accidentally uploaded earlier in the week...

--- loading/automated/stats/reset.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /root/webserver/scratch/loading/automated/stats/
-
-echo "Error Output File:" > ./out.log
-echo "Process File:" > ./pid.txt
-echo "Current Run File:" > ./run.txt
-echo "Extraneous Output File:" > ./waste.txt
