8d40ee8b29ba98f03603229b5551ed5eb6519dbc
Joseph ONeill <oneilljo@appstate.edu>
2018-09-18 11:14:17 -0600

    Finalized all the upfront processing (finally). Projects can now be submitted using multiple variations of the project URL and ID successfully.

--- loading/download.php
+++ loading/download.php
@@ -11,60 +11,22 @@
   <script defer>
   function download() {
   var projectID = "<?php echo $_POST['project']?>";
-  var elem = document.getElementById("myBar");
-  var width = 1;
-  var id = setInterval(frame, 30);
-  var progress = 0;
   <?php exec("./projectdownloader.py " . $_POST["project"]); ?>
-  progress = progress + 10;
   <?php exec("chmod 777 project-" . $_POST["project"] . ".sb2"); ?>
-  progress = progress + 10;
   <?php exec("chown apache:apache project-" . $_POST["project"] . ".sb2"); ?>
-  progress = progress + 10;
   <?php exec("cp project-" . $_POST["project"] . ".sb2 ../savedProjects"); ?>
-  progress = progress + 10;
   <?php exec("mv -f project-" . $_POST["project"] . ".sb2 ../SCATT/submissions/project-" . $_POST["project"] . ".sb2"); ?>
-  progress = progress + 10;
   <?php exec("/usr/bin/java -jar ../SCATT/Scatt.jar"); ?>
-  progress = progress + 10;
   <?php exec("rm -f ../SCATT/submissions/project-" . $_POST["project"] . ".sb2"); ?>
-  progress = progress + 10;
   <?php exec("rm -rf zips/" . $_POST["project"] . ".zip"); ?>
-  progress = progress + 10;
   <?php exec("mv unzips/project-" . $_POST["project"] . "/project.json ../savedReports/project-" . $_POST["project"]); ?>
-  progress = progress + 10;
   <?php exec("rm -rf unzips/" . $_POST["project"]); ?>
-  progress = progress + 10;
-
-  function frame() {
-    if (width >= 100) {
-      clearInterval(id);
-      window.location = "../progress/index.php?projectid=" + projectID;
-    } else {
-      width = progress;
-      elem.style.width = width + '%';
-    }
+  window.location = "../progress/index.php?projectid=" + projectID;
   }
-  }
-  download();
   </script>
 </head>
-<body>
-  <div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
-    <div class="w3-display-topright w3-padding-small w3-medium">
-      <a href="../">Home</a> | <a href="../about/">About</a> | <a href="../login/">Login</a> | <a href="../signup/">Sign Up</a>
-    </div>
-    <div class="w3-display-middle w3-center">
-      <img class= "w3-animate-top" src="../images/scratch.png" alt = "Title">
-      </br>
-      <div class="w3-light-grey w3-round-xlarge w3-animate-bottom">
-        <!--<div id="myBar" class="w3-container w3-blue w3-round-xlarge" style="height:24px;width:1%"></div>-->
-	<img class= "w3-animate-top" src="../images/loading.gif" alt = "Loading">
-      </div>
-    </div>
-    <div class="w3-display-bottomleft w3-padding-small w3-tiny">
-    Created &amp; Mantained by Joseph O'Neill. Website Repository can be found <a href="https://github.com/oneilljo/SCRATCH_Analysis" target="_blank">here</a>.
-    </div>
+<body onload="download()">
+  <div class="bgimg">
   </div>
 </body>
 </html>
